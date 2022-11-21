ceb0e727c2c292c4e8766e875b1d74b62c032e95
yuzgec <olcayy@gmail.com>
2022-11-21 04:59:02 +0300

    Fix

---
+++ resources/views/elysiumtaki/frontend/layout/header.blade.php
@@ -0,0 +1,304 @@
+<header class="header">
+    <div class="header-top">
+        <div class="container">
+            <div class="header-left">
+                <p class="welcome-msg">Welcome to Wolmart Store message or remove it!</p>
+            </div>
+            <div class="header-right">
+                <div class="dropdown">
+                    <a href="#currency">USD</a>
+                    <div class="dropdown-box">
+                        <a href="#USD">USD</a>
+                        <a href="#EUR">EUR</a>
+                    </div>
+                </div>
+                <!-- End of DropDown Menu -->
+                <div class="dropdown">
+                    <a href="#language"><img src="assets/images/flags/eng.png" alt="ENG Flag" width="14"
+                                             height="8" class="dropdown-image" /> ENG</a>
+                    <div class="dropdown-box">
+                        <a href="#ENG">
+                            <img src="assets/images/flags/eng.png" alt="ENG Flag" width="14" height="8"
+                                 class="dropdown-image" />
+                            ENG
+                        </a>
+                        <a href="#FRA">
+                            <img src="assets/images/flags/fra.png" alt="FRA Flag" width="14" height="8"
+                                 class="dropdown-image" />
+                            FRA
+                        </a>
+                    </div>
+                </div>
+                <!-- End of Dropdown Menu -->
+                <span class="divider d-lg-show"></span>
+                <a href="blog.html" class="d-lg-show">Sell on Wolmart</a>
+                <a href="my-account.html" class="d-lg-show">My Account</a>
+                <a href="assets/ajax/login.html" class="d-lg-show login sign-in"><i
+                        class="w-icon-account"></i>Sign In</a>
+                <span class="delimiter d-lg-show">/</span>
+                <a href="assets/ajax/login.html" class="ml-0 d-lg-show login register">Register</a>
+            </div>
+        </div>
+    </div>
+    <!-- End of Header Top -->
+    <div class="header-middle sticky-content fix-top sticky-header border-no">
+        <div class="container">
+            <div class="header-left mr-md-4">
+                <a href="#" class="mobile-menu-toggle  w-icon-hamburger"></a>
+                <a href="demo12.html" class="logo">
+                    <img src="assets/images/demos/demo12/logo.png" alt="logo" width="144" height="45">
+                </a>
+                <form method="get" action="#"
+                      class="input-wrapper header-search hs-expanded hs-round d-none d-md-flex">
+                    <div class="select-box bg-white">
+                        <select id="category" name="category">
+                            <option value="">All Categories</option>
+                            <option value="4">Fashion</option>
+                            <option value="5">Furniture</option>
+                            <option value="6">Shoes</option>
+                            <option value="7">Sports</option>
+                            <option value="8">Games</option>
+                            <option value="9">Computers</option>
+                            <option value="10">Electronics</option>
+                            <option value="11">Kitchen</option>
+                            <option value="12">Clothing</option>
+                        </select>
+                    </div>
+                    <input type="text" class="form-control bg-white pt-0 pb-0" name="search" id="search"
+                           placeholder="Search in..." required />
+                    <button class="btn btn-search" type="submit">
+                        <i class="w-icon-search"></i>
+                    </button>
+                </form>
+            </div>
+            <div class="header-right ml-4">
+                <div class="header-call d-xs-show d-lg-flex align-items-center">
+                    <a href="tel:#" class="w-icon-call"></a>
+                    <div class="call-info d-lg-show">
+                        <h4 class="chat font-size-md text-normal ls-normal text-white mb-0">
+                            <a href="mailto:#" class="text-capitalize text-primary font-weight-normal">Live
+                                Chat</a>
+                            <span class="text-light font-weight-normal">or :</span>
+                        </h4>
+                        <a href="tel:#" class="phone-number font-weight-bolder ls-50">0(800)123-456</a>
+                    </div>
+                </div>
+                <a class="wishlist label-down link d-xs-show" href="wishlist.html">
+                    <i class="w-icon-heart"></i>
+                    <span class="wishlist-label d-lg-show">Wishlist</span>
+                </a>
+                <a class="compare label-down link d-xs-show" href="compare.html">
+                    <i class="w-icon-compare"></i>
+                    <span class="compare-label d-lg-show">Compare</span>
+                </a>
+                <div class="dropdown cart-dropdown cart-offcanvas mr-0 mr-lg-2">
+                    <div class="cart-overlay"></div>
+                    <a href="#" class="cart-toggle label-down link">
+                        <i class="w-icon-cart">
+                            <span class="cart-count text-white">2</span>
+                        </i>
+                        <span class="cart-label">Cart</span>
+                    </a>
+                    <div class="dropdown-box">
+                        <div class="cart-header">
+                            <span>Shopping Cart</span>
+                            <a href="#" class="btn-close">Close<i class="w-icon-long-arrow-right"></i></a>
+                        </div>
+                        <div class="products">
+                            <div class="product product-cart">
+                                <div class="product-detail">
+                                    <a href="product-default.html" class="product-name">Beige knitted
+                                        elas<br>tic
+                                        runner shoes</a>
+                                    <div class="price-box">
+                                        <span class="product-quantity">1</span>
+                                        <span class="product-price">$25.68</span>
+                                    </div>
+                                </div>
+                                <figure class="product-media">
+                                    <a href="product-default.html">
+                                        <img src="assets/images/cart/product-1.jpg" alt="product" height="84"
+                                             width="94">
+                                    </a>
+                                </figure>
+                                <button class="btn btn-link btn-close">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                            <div class="product product-cart">
+                                <div class="product-detail">
+                                    <a href="product-default.html" class="product-name">Blue utility
+                                        pina<br>fore
+                                        denim dress
+                                    </a>
+                                    <div class="price-box">
+                                        <span class="product-quantity">1</span>
+                                        <span class="product-price">$32.99</span>
+                                    </div>
+                                </div>
+                                <figure class="product-media">
+                                    <a href="product-default.html">
+                                        <img src="assets/images/cart/product-2.jpg" alt="product" width="84"
+                                             height="94">
+                                    </a>
+                                </figure>
+                                <button class="btn btn-link btn-close">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="cart-total">
+                            <label>Subtotal:</label>
+                            <span class="price">$58.67</span>
+                        </div>
+                        <div class="cart-action">
+                            <a href="cart.html" class="btn btn-dark btn-outline btn-rounded">View Cart</a>
+                            <a href="checkout.html" class="btn btn-primary  btn-rounded">Checkout</a>
+                        </div>
+                    </div>
+                    <!-- End of Dropdown Box -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End of Header Middle -->
+</header>
+
+<header class="header">
+
+    <div class="header-middle">
+        <div class="container">
+            <div class="header-left mr-md-4">
+                <a href="#" class="mobile-menu-toggle  w-icon-hamburger" aria-label="menu-toggle">
+                </a>
+                <a href="{{ route('home') }}" class="logo ml-lg-0">
+                    <img src="/frontend/images/{{ config('app.tema') }}/logo.png" alt="{{ config('app.name') }}" width="200"  />
+                </a>
+
+                <form action="{{ route('search') }}" method="GET"
+                      class="input-wrapper header-search hs-expanded hs-round d-none d-md-flex">
+                    <div class="select-box">
+                        <select id="category" name="category">
+                            <option value="">Tüm Kategoriler</option>
+                            @foreach($Product_Categories->where('parent_id' , 0) as $item)
+                                <option value="{{ $item->id }}">{{ $item->title }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <input type="text" class="form-control" name="q" id="search" placeholder="Ürün Adı veya kodu giriniz..."
+                           required />
+                    <button class="btn btn-search" type="submit"><i class="w-icon-search"></i>
+                    </button>
+                </form>
+            </div>
+            <div class="header-right ml-4">
+                <div class="header-call d-xs-show d-lg-flex align-items-center">
+                    <a href="tel:#" class="w-icon-call"></a>
+                    <div class="call-info d-lg-show">
+                        <h4 class="chat font-weight-normal font-size-md text-normal ls-normal text-light mb-0">
+                            <a href="mailto:#" class="text-capitalize">Sipariş Hattı</a></h4>
+                        <a href="tel:#" class="phone-number font-weight-bolder ls-50">{{ config('settings.telefon1') }}</a>
+                    </div>
+                </div>
+
+                <a class="wishlist label-down link d-xs-show" href="{{ route('login') }}">
+                    <i class="w-icon-user"></i>
+                    <span class="wishlist-label d-lg-show">Giriş Yap </span>
+                </a>
+
+                <a class="wishlist label-down link d-xs-show" href="{{ route('favori') }}">
+                    <i class="w-icon-heart"></i>
+                    <span class="wishlist-label d-lg-show">Favori</span>
+                </a>
+
+                <div class="dropdown cart-dropdown cart-offcanvas mr-0 mr-lg-2">
+                    <div class="cart-overlay"></div>
+                    <a href="#" class="cart-toggle label-down link">
+                        <i class="w-icon-cart">
+                            <span class="cart-count">{{ Cart::instance('shopping')->content()->count() }}</span>
+                        </i>
+                        <span class="cart-label">Sepet</span>
+                    </a>
+                    <div class="dropdown-box">
+                        <div class="cart-header">
+                            <span>Sepetim</span>
+                            <a href="#" class="btn-close">Kapat<i class="w-icon-long-arrow-right"></i></a>
+                        </div>
+                        @foreach(Cart::content() as $c)
+                        <div class="products">
+                            <div class="product product-cart">
+                                <div class="product-detail">
+                                    <a href="{{ route('urun', $c->options->url) }}" class="product-name">{{$c->name}}</a>
+
+                                    <div class="price-box">
+                                        <span class="product-quantity">{{$c->qty}}</span>
+                                        <span class="product-price">{{$c->price}}</span>
+                                    </div>
+                                </div>
+                                <figure class="product-media">
+                                    <a href="{{ route('urun', $c->options->url) }}" class="product-image">
+                                        <img src="{{ $c->options->image }}" alt="{{$c->name}}" height="80px">
+                                    </a>
+                                </figure>
+                                <form id="form" method="post" action="{{route('sepetcikar',$c->rowId )}}">
+                                @csrf
+                                <a href="javascript:{}" onclick="document.getElementById('form').submit()" class="btn btn-link btn-close" aria-label="button">
+                                    <i class="fas fa-times"></i>
+                                </a>
+                                </form>
+                            </div>
+                        </div>
+                        @endforeach
+                        <div class="cart-total">
+                            <label>Ara Toplam:</label>
+                            <span class="price">Toplam</span>
+                        </div>
+
+                        <div class="cart-action">
+                            <a href="{{ route('sepet') }}" class="btn btn-dark btn-outline btn-rounded btn-block">Sepetim</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="header-bottom sticky-content fix-top sticky-header">
+        <div class="container">
+            <div class="inner-wrap">
+                <div class="header-left">
+                    <div class="dropdown category-dropdown gold {{ (request()->segment(1)) ? 'show' : 'show-dropdown' }}" data-visible="true">
+                        <a href="#" class="category-toggle" role="button" data-toggle="dropdown"
+                           aria-haspopup="true" aria-expanded="true" data-display="static"
+                           title="Browse Categories">
+                            <i class="w-icon-category"></i>
+                            <span>KATEGORİLERİMİZ </span>
+                        </a>
+                        <div class="dropdown-box">
+                            <ul class="menu vertical-menu category-menu">
+                                @include(config('app.tema').'/frontend.layout.sidemenu')
+                            </ul>
+                        </div>
+                    </div>
+
+                    <nav class="main-nav ml-4">
+                        <ul class="menu active-underline">
+                            <li class="active"><a href="{{ route('home') }}">Anasayfa</a></li>
+                            <li class=""><a href="{{ route('home') }}">Hakkımızda</a></li>
+                            <li class=""><a href="{{ route('home') }}">Sizden Gelenler</a></li>
+                            <li class=""><a href="{{ route('kargosorgulama') }}">Kampanyalarımız</a></li>
+                            <li class=""><a href="{{ route('iletisim') }}">Bize Ulaşın</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div class="header-right">
+                    <a href="{{ route('kargosorgulama') }}" class="d-xl-show"><i class="w-icon-map-marker mr-1"></i>Kargo Sorgulama</a>
+                    <a href="#"><i class="w-icon-sale"></i>Günün Fırsatı</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</header>
---
+++ resources/views/elysiumtaki/frontend/layout/sidemenu.blade.php
@@ -0,0 +1,7 @@
+@foreach($Product_Categories as $item)
+    <li><a href="{{ route('kategori', [$item->slug,'id' => $item->id]) }}">{{ $item->title }}</a></li>
+@endforeach
+<li><a href="{{ route('home') }}">Kampanyalı Ürünler</a></li>
+<li><a href="{{ route('home') }}">İndirimli Ürünler</a></li>
+<li><a href="{{ route('home') }}">Çok Satanlar</a></li>
+<li><a href="{{ route('home') }}">69TL Altı Ürünler</a></li>
