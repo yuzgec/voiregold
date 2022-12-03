55ad94496bcea9c2a4ae23cbb3e62a33308cdbae
yuzgec <olcayy@gmail.com>
2022-12-03 10:39:29 +0300

    Fix

--- resources/views/suavebuji/frontend/index.blade.php
+++ resources/views/suavebuji/frontend/index.blade.php
@@ -168,9 +168,167 @@
                     <x-shop.product-item :item="$item"/>
                 </div>
 
+
+
             @endforeach
         </div>
+            @if($loop->index == 1 )
 
+                <div class="sale-banner banner br-sm appear-animate fadeIn appear-animation-visible" style="animation-duration: 1.2s;">
+                    <div class="banner-content">
+                        <h4 class="content-left banner-subtitle text-uppercase mb-8 mb-md-0 mr-0 mr-md-4 text-secondary ls-25">
+                            <span class="text-dark font-weight-bold lh-1 ls-normal">Voire
+                                <br>Gold</span>50% İNDİRİM!</h4>
+                        <div class="content-right">
+                            <h3 class="banner-title text-uppercase font-weight-normal mb-4 mb-md-0 ls-25 text-dark">
+                                <span>TÜM ÜRÜNLERİMİZDE
+                                    <strong class="mr-10 pr-lg-10">%50'ye varan İndirim Fırsatı</strong>
+                                    TÜM ÜRÜNLERİMİZDE
+                                    <strong class="mr-10 pr-lg-10">%50'ye varan İndirim Fırsatı</strong>
+                                    TÜM ÜRÜNLERİMİZDE
+                                    <strong class="mr-10 pr-lg-10">%50'ye varan İndirim Fırsatı</strong>
+                                </span>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+
+            @endif
+
+            @if($loop->index == 3 )
+                <div class="row deals-wrapper appear-animate mb-8">
+                    <div class="col-xl-9 mb-4">
+                        <div class="single-product h-100 br-sm">
+                            <h4 class="title-sm title-underline font-weight-bolder ls-normal">
+                                Günün İndirimli Ürünleri
+                            </h4>
+                            <div class="swiper">
+                                <div class="swiper-container swiper-theme nav-top swiper-nav-lg" data-swiper-options = "{
+                                            'spaceBetween': 20,
+                                            'slidesPerView': 1
+                                        }">
+                                    <div class="swiper-wrapper row cols-1 gutter-no">
+                                        @foreach($Product->where('opportunity', 1) as $item)
+                                            <div class="swiper-slide">
+                                                <div class="product product-single row">
+                                                    <div class="col-md-6">
+                                                        <div class="product-gallery product-gallery-sticky product-gallery-vertical">
+                                                            <div class="swiper-container product-single-swiper swiper-theme nav-inner">
+                                                                <div class="swiper-wrapper row cols-1 gutter-no">
+                                                                    <div class="swiper-slide" style="border:1px solid #f4f4f4;border-radius:5px">
+                                                                        <figure class="product-image">
+                                                                            <img title="{{ $item->title }}" src="{{ (!$item->getFirstMediaUrl('page')) ? '/resimyok.jpg' : $item->getFirstMediaUrl('page', 'thumb')}}">
+                                                                        </figure>
+                                                                    </div>
+
+                                                                </div>
+
+                                                                <div class="product-label-group">
+                                                                    <label class="product-label gold">%{{ abs(round( $item->price * 100 /$item->old_price - 100)) }} indirim</label>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="product-details scrollable pl-0">
+                                                            <h2 class="product-title mb-1">
+                                                                <a href="{{ route('urun' , $item->slug)}}" title="{{ $item->title }}">
+                                                                    {{ $item->title }}
+                                                                </a>
+                                                            </h2>
+
+                                                            <hr class="product-divider">
+
+                                                            <div class="product-price">
+                                                                <ins class="new-price ls-50"> {{ $item->price }}₺ -
+                                                                    <del>@convert($item->old_price)₺</del>
+                                                                </ins>
+                                                            </div>
+
+                                                            @if($item->get_comment_count > 0)
+                                                                <div class="ratings-container">
+                                                                    <div class="ratings-full">
+                                                                        <span class="ratings" style="width: 100%;"></span>
+                                                                        <span class="tooltiptext tooltip-top"></span>
+                                                                    </div>
+                                                                    <a href="{{ route('urun' , $item->slug)}}" title="{{ $item->title }}" class="rating-reviews">({{ $item->get_comment_count }} yorum)</a>
+                                                                </div>
+                                                            @endif
+
+                                                            <div class="product-short-desc lh-2 short" style="font-size: 16px">
+                                                                {!! $item->short !!}
+                                                            </div>
+
+                                                            <div class="product-form pt-4">
+                                                                <a href="{{ route('urun' , $item->slug)}}" class="btn btn-primary" title="{{ $item->title }}">
+                                                                    <i class="w-icon-cart"></i>
+                                                                    <span>Ürünü İncele</span>
+                                                                </a>
+                                                            </div>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        @endforeach
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 mb-4">
+                        <div class="widget widget-products widget-products-bordered h-100">
+                            <div class="widget-body br-sm h-100">
+                                <h4 class="title title-link font-weight-bold">En Çok Satanlar</h4>
+                                <div class="swiper">
+                                    <div class="swiper nav-top">
+                                        <div class="swiper-container swiper-theme nav-top" data-swiper-options = "{
+                                                'slidesPerView': 1,
+                                                'spaceBetween': 20,
+                                                'navigation': {
+                                                    'prevEl': '.swiper-button-prev',
+                                                    'nextEl': '.swiper-button-next'
+                                                }
+                                            }">
+                                            <div class="swiper-wrapper">
+                                                <div class="widget-col swiper-slide">
+                                                    @foreach($Product->where('bestselling', 1) as $item)
+                                                        <div class="product product-widget">
+                                                            <figure class="product-media">
+                                                                <a href="{{ route('urun' , $item->slug)}}" title="{{ $item->title }}">
+                                                                    <img class="img-fluid" src="{{ (!$item->getFirstMediaUrl('page')) ? '/resimyok.jpg' : $item->getFirstMediaUrl('page', 'small') }}" alt="{{ $item->title }}" width="150" height="150">
+                                                                    @foreach($item->getMedia('gallery')->take(1) as $img)
+                                                                        {{ $img->img('small')->attributes(['class' => 'product-image-hover', 'alt' => $item->title]) }}
+                                                                    @endforeach
+                                                                </a>
+                                                            </figure>
+                                                            <div class="product-details">
+                                                                <h4 class="product-name">
+                                                                    <a href="{{ route('urun' , $item->slug)}}" title="{{ $item->title }}">{{ $item->title }}</a>
+                                                                </h4>
+                                                                <div class="ratings-container">
+                                                                    <div class="ratings-full">
+                                                                        <span class="ratings" style="width: 100%;"></span>
+                                                                        <span class="tooltiptext tooltip-top"></span>
+                                                                    </div>
+                                                                </div>
+                                                                <div class="product-price">@convert($item->price)₺ - <del>@convert($item->old_price)₺</del></div>
+                                                            </div>
+                                                        </div>
+                                                    @endforeach
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endif
         @endforeach
     </div>
 @endsection
