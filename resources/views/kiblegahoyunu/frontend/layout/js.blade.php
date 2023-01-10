918467c3ca96b62e17dbbc12ef7d8775282c3a1a
yuzgec <olcayy@gmail.com>
2023-01-10 17:48:48 +0300

    Fix

--- resources/views/kiblegahoyunu/frontend/layout/js.blade.php
+++
@@ -1,5 +0,0 @@
-<script src="/kiblegah/js/vendor/jquery-1.12.0.min.js"></script>
-<script src="/kiblegah/js/popper.js"></script>
-<script src="/kiblegah/js/bootstrap.min.js"></script>
-<script src="/kiblegah/js/plugins.js"></script>
-<script src="/kiblegah/js/main.js"></script>
