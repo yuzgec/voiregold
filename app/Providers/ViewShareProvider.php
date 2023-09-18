b4e60108fe5c348803ded70195255a2892c615a7
yuzgec <olcayy@gmail.com>
2023-09-18 14:18:10 +0300

    Fix

--- app/Providers/ViewShareProvider.php
+++ app/Providers/ViewShareProvider.php
@@ -37,7 +37,7 @@ class ViewShareProvider extends ServiceProvider
             $Product = Product::with(['getCategory'])
                 ->select('id', 'title', 'price', 'old_price', 'slug','bestselling','status','sku','offer','short', 'opportunity')
                 ->where('status',1)
-                ->orderBy('created_at','desc')
+                ->orderBy('created_at','asc')
                 ->get();
 
             View::share([
