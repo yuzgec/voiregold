76b4236dba1d65c67e20ab40a7f91c15bc2c210a
yuzgec <olcayy@gmail.com>
2024-08-06 23:12:39 +0300

    Fix

--- resources/views/suavebuji/frontend/layout/sidemenu.blade.php
+++ resources/views/suavebuji/frontend/layout/sidemenu.blade.php
@@ -1,7 +1,5 @@
 @foreach($Product_Categories as $item)
     <li><a href="{{ route('kategori', [$item->slug,'id' => $item->id]) }}">{{ $item->title }}</a></li>
 @endforeach
-<li><a href="{{ route('home') }}">Kampanyalı Ürünler</a></li>
 <li><a href="{{ route('home') }}">İndirimli Ürünler</a></li>
 <li><a href="{{ route('home') }}">Çok Satanlar</a></li>
-<li><a href="{{ route('home') }}">69TL Altı Ürünler</a></li>
